b9162080e90d379771bc176dbdc344b93ab544d9
banerjeesatwik2000@gmail.com <banerjeesatwik2000@gmail.com>
2024-06-07 00:43:19 +0530

    getAllEmployee and saveEmployeeTaskToTaskList methods added in the KanbanServiceImpl class

--- kanban-be/KanbanBoardService/src/main/java/com/bej/service/IKanbanService.java
+++ kanban-be/KanbanBoardService/src/main/java/com/bej/service/IKanbanService.java
@@ -2,11 +2,14 @@ package com.bej.service;
 
 import com.bej.domain.Employee;
 import com.bej.domain.Task;
+import com.bej.exception.EmployeeNotFoundException;
+import com.bej.exception.TaskAlreadyExistsException;
+import com.bej.exception.TaskNotFoundException;
 
 import java.util.List;
 
 public interface IKanbanService
 {
-    List<Employee> getAllEmployee(String userId);
-    Employee saveEmployeeTaskToTaskList(Task task, String userId);
+    List<Employee> getAllEmployee(String userId) throws Exception;
+    Employee saveEmployeeTaskToTaskList(Task task, String userId) throws EmployeeNotFoundException , TaskAlreadyExistsException;
 }
--- kanban-be/KanbanBoardService/src/main/java/com/bej/service/KanbanServiceImpl.java
+++ kanban-be/KanbanBoardService/src/main/java/com/bej/service/KanbanServiceImpl.java
@@ -1,7 +1,26 @@
 package com.bej.service;
 
+import com.bej.domain.Employee;
+import com.bej.domain.Task;
+import com.bej.exception.EmployeeNotFoundException;
+import com.bej.exception.TaskAlreadyExistsException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class KanbanServiceImpl {
+public class KanbanServiceImpl implements IKanbanService
+{
+
+    @Override
+    public List<Employee> getAllEmployee(String userId) throws Exception
+    {
+
+        return null;
+    }
+
+    @Override
+    public Employee saveEmployeeTaskToTaskList(Task task, String userId) throws EmployeeNotFoundException, TaskAlreadyExistsException {
+        return null;
+    }
 }
