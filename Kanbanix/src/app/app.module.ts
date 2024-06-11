ab68039af6c2118d2280cf77078da07a7e601b5b
banerjeesatwik2000@gmail.com <banerjeesatwik2000@gmail.com>
2024-06-11 20:16:39 +0530

    Navbar component added

--- Kanbanix/src/app/app.module.ts
+++ Kanbanix/src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [
     provideAnimationsAsync()
