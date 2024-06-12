d6b54e4bc48758fcb5f8d05cfc9baec113dbc74b
banerjeesatwik2000@gmail.com <banerjeesatwik2000@gmail.com>
2024-06-12 10:50:55 +0530

    Manager registration page designed

--- Kanbanix/src/app/app.module.ts
+++ Kanbanix/src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms'; 
+
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ManagerRegisterComponent } from './manager-register/manager-register.component';
+import { EmployeeRegisterComponent } from './employee-register/employee-register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HomePageComponent,
-    NavbarComponent
+    NavbarComponent,
+    ManagerRegisterComponent,
+    EmployeeRegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideAnimationsAsync()
---
+++ Kanbanix/src/app/manager-register/manager-register.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Manager }  from '../../Models/Manager';
+
+
+@Component({
+  selector: 'app-manager-register',
+  templateUrl: './manager-register.component.html',
+  styleUrl: './manager-register.component.css'
+})
+export class ManagerRegisterComponent {
+  registrationForm: FormGroup = new FormGroup({});
+  constructor
+  (private formBuilder : FormBuilder){}
+ 
+  ngOnInit(): void {
+    this.registrationForm = this.formBuilder.group({
+      managerId: [''],
+      managerName: ['', [Validators.required, Validators.minLength(20), Validators.pattern(/^[A-Za-z]+$/)]],
+      managerEmail: ['', [Validators.required]],
+      managerPassword: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    }, { validator: this.passwordMatchValidator });
+  }
+  onSubmit() {
+    console.log(this.registrationForm.valid);
+    console.log(this.registrationForm.value);
+    if (this.registrationForm.valid) {
+      const manager: Manager ={
+        managerId: this.registrationForm.value.managerId,
+        managerName: this.registrationForm.value.managerName,
+        managerEmail: this.registrationForm.value.managerEmail,
+        managerPassword: this.registrationForm.value.managerPassword
+      }
+    }
+    
+  }
+  passwordMatchValidator(formGroup: FormGroup){
+    const password = formGroup.get('managerPassword')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : {passwordMatch: true};
+  }
+  }
+
+ 
+
+
+
+
+
