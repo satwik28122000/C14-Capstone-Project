546a452da9e2e4520e9875fb146d436f59e62c81
pallavi0930 <kapoorpallavi0421@gmail.com>
2024-06-14 01:06:07 +0530

    managerLogin working

--- Kanbanix/src/app/manager-login/manager-login.component.ts
+++ Kanbanix/src/app/manager-login/manager-login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Manager } from '../../models/manager';
+import { RouterService } from '../services/router.service';
+import { ManagerService } from '../services/manager.service';
 
 @Component({
   selector: 'app-manager-login',
@@ -11,7 +13,7 @@ export class ManagerLoginComponent {
   loginForm: FormGroup=new FormGroup ({});
   manager: Manager ={};
   constructor
-  (private formBuilder : FormBuilder){}
+  (private formBuilder : FormBuilder,private routerService:RouterService,private managerService:ManagerService){}
 
   ngOnInit(): void {
       this.loginForm = this.formBuilder.group({
@@ -21,15 +23,27 @@ export class ManagerLoginComponent {
   }
 
   onSubmit(form: NgForm) {
-      console.log(this.loginForm.valid);
-      console.log(this.loginForm.value);
-      if (this.loginForm.valid) {
-            const manager: Manager ={
-              managerId: this.loginForm.value.managerId,
-              managerPassword: this.loginForm.value.Password
-            }
-          }
+      // console.log(this.loginForm.valid);
+      // console.log(this.loginForm.value);
+      // if (this.loginForm.valid) {
+      //       const manager: Manager ={
+      //         managerId: this.loginForm.value.managerId,
+      //         managerPassword: this.loginForm.value.Password
+      //       }
+      //     }
+      this.managerService.loginManager(form.value).subscribe({
+        
+        next: res => {
+          console.log(res);
+          this.routerService.redirectToManagerView(form.value?.managerId);
+        },
+        error: err =>{
+          console.log(form.value);
+          console.log(err);
         }
+      })
+
+  }
              passwordMatchValidator(formGroup: FormGroup){
           const password = formGroup.get('managerPassword')?.value;
           const confirmPassword = formGroup.get('confirmPassword')?.value;
