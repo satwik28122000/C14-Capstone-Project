1126f1b6583be2fc4cba7256ccf94f6568a41745
pallavi0930 <kapoorpallavi0421@gmail.com>
2024-06-13 12:07:23 +0530

    CrossOrigin added

--- kanban-be/UserAuthentication/src/main/java/com/bej/controller/UserAuthController.java
+++ kanban-be/UserAuthentication/src/main/java/com/bej/controller/UserAuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 public class UserAuthController {
