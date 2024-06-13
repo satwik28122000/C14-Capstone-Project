78ec4b559f4afd2e9ca68072be05ae1b57a9e754
banerjeesatwik2000@gmail.com <banerjeesatwik2000@gmail.com>
2024-06-13 23:57:09 +0530

    claims added in getAllEmployeeTaskFromTaskList method

--- Kanbanix/src/app/manager-register/manager-register.component.ts
+++ Kanbanix/src/app/manager-register/manager-register.component.ts
@@ -1,59 +1,3 @@
-// import { Component } from '@angular/core';
-// import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { Manager } from '../../models/manager';
-
-
-
-// @Component({
-//   selector: 'app-manager-register',
-//   templateUrl: './manager-register.component.html',
-//   styleUrl: './manager-register.component.css'
-// })
-// export class ManagerRegisterComponent {
-//   registrationForm: FormGroup = new FormGroup({});
-//   constructor
-//   (private formBuilder : FormBuilder){}
- 
-//   ngOnInit(): void {
-//     this.registrationForm = this.formBuilder.group({
-//       managerId: ['', [Validators.required]],
-//       managerName: ['', [Validators.required, Validators.minLength(10), Validators.pattern("/^[A-Z a-z]+$/")]],
-//       managerEmail: ['', [Validators.required]],
-//       managerPassword: ['', [Validators.required, Validators.minLength(6)]],
-//       confirmPassword: ['', [Validators.required]]
-//     }, { validators: this.passwordMatchValidator });
-//   }
-//   onSubmit() {
-//     console.log(this.registrationForm.valid);
-//     console.log(this.registrationForm.value);
-//     if (this.registrationForm.valid) {
-//       const manager: Manager ={
-//         managerId: this.registrationForm.value.managerId,
-//         managerName: this.registrationForm.value.managerName,
-//         managerEmail: this.registrationForm.value.managerEmail,
-//         managerPassword: this.registrationForm.value.managerPassword
-//       }
-//     }
-    
-//   }
-
-//   passwordMatchValidator(ac:AbstractControl){
-//     let password=ac.get('managerPassword')?.value;
-//     let confirmPassword=ac.get('confirmPassword')?.value;
-//     console.log(password+" "+confirmPassword);
-//     if(password!==confirmPassword)
-//       {
-//         return {pError:true};
-//       }else{
-//         return null;
-//       }
-
-    
-//   }
-
-  
-//   }
-
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Manager } from '../../models/manager';
@@ -92,21 +36,7 @@ export class ManagerRegisterComponent implements OnInit {
         }
       })
   }
-  // passwordMatchValidator(formGroup: FormGroup){
-  //   const password = formGroup.get('managerPassword')?.value;
-  //   const confirmPassword = formGroup.get('confirmPassword')?.value;
-  //   return password === confirmPassword ? null : {passwordMatch: true};
-  // }
   
-    // if (this.registrationForm.valid) {
-    //   const manager: Manager = {
-    //     managerId: this.registrationForm.value.managerId,
-    //     managerName: this.registrationForm.value.managerName,
-    //     managerEmail: this.registrationForm.value.managerEmail,
-    //     managerPassword: this.registrationForm.value.managerPassword
-    //   };
-    //   // Process the manager object as needed
-    // }
   
 
   passwordMatchValidator(ac: AbstractControl) {
