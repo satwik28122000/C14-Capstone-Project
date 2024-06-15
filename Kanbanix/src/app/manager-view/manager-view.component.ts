600c1c478d544a701923e688bf272fbe4cdfbd74
pallavi0930 <kapoorpallavi0421@gmail.com>
2024-06-15 13:33:37 +0530

    ProjectCard routing to ProjectTaskView

--- Kanbanix/src/app/manager-view/manager-view.component.ts
+++ Kanbanix/src/app/manager-view/manager-view.component.ts
@@ -22,9 +22,10 @@ export class ManagerViewComponent implements OnInit {
       next: data => {
         const managerId = data.get('id') ?? "";
         this.managerService.fetchAllManager().subscribe({
-          next: res =>{
+          next: (res:any) =>{
             console.log(res);
-            this.manager = res.find(m => m.managerId === managerId);
+            this.manager = res.find((m: { managerId: string; }) => m.managerId === managerId);
+            localStorage.setItem("Token",res.Token)
             console.log(this.manager);
             
           },
